1f3ec35da288a82fe92c15444da5f9896373ee38
Michael Fladischer <FladischerMichael@fladi.at>
2013-01-23 08:08:15 +0100

    Provide pligun.zsh file for antigen integration.

---
+++ dircolors.plugin.zsh
@@ -0,0 +1,8 @@
+eval $(dircolors $location/dircolors.${DIRCOLORS_THEME:-256dark})
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+
+# List direcory contents
+export LS_OPTIONS='--color'
+alias l='ls $LS_OPTIONS'
+alias ll='ls $LS_OPTIONS -lh'
+alias lll='ls $LS_OPTIONS -alh'
